d38808c60be6129bd2515d07290b616abd04ea57
Alexandre Quiblier <alexandre.quiblier@meona.de>
2022-04-09 20:16:08 +0200

    running metamask accounts on ganache

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+require("dotenv").config({path:  "./.env"});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
+
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -6,7 +10,15 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 7545
+      port: 7545,
+      host: "127.0.0.1",
+      network_id: 5777
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex)
+      },
+      network_id: 5777
     }
   },
   compilers: {
