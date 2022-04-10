39ff7ac0540be02a2a643233df72c80bf828e61d
Alexandre Quiblier <alexandre.quiblier@meona.de>
2022-04-10 11:01:56 +0200

    TOken Sell Ready to deply

--- truffle-config.js
+++ truffle-config.js
@@ -19,6 +19,18 @@ module.exports = {
         return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", MetaMaskAccountIndex )
       },
       network_id: 5777
+    },    
+     goerli_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/e7990eb1349f416d9e8c26766e59ad13", MetaMaskAccountIndex )
+      },
+      network_id: 5
+    },     
+     ropsten_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/e7990eb1349f416d9e8c26766e59ad13", MetaMaskAccountIndex )
+      },
+      network_id: 3
     }
   },
   compilers: {
